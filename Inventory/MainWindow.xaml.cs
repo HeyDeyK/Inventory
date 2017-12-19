c06c36a0e364cfb63b939c132d16ef9605033dc5
vileton15 <vileton15@sps-prosek.cz>
2017-12-19 10:43:44 +0100

    fg

--- Inventory/MainWindow.xaml.cs
+++ Inventory/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FileHelpers;
+using System.IO;
 
 namespace Inventory
 {
@@ -20,9 +22,57 @@ namespace Inventory
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isRectDragInProg;
         public MainWindow()
         {
             InitializeComponent();
+            double[] nazvy = new double[5];
+            rect1.SetValue(Grid.RowProperty, 0);
+
         }
+
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = true;
+            var rectangle = sender as System.Windows.Shapes.Rectangle;
+            rectangle.CaptureMouse();
+        }
+
+
+        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = false;
+            var rectando = sender as System.Windows.Shapes.Rectangle;
+            rectando.ReleaseMouseCapture();
+            
+
+        }
+
+        private void rect_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        { 
+            var rectando = sender as System.Windows.Shapes.Rectangle;
+            if (!_isRectDragInProg) return;
+
+            // get the position of the mouse relative to the Canvas
+            var mousePos = e.GetPosition(canvas);
+
+            // center the rect on the mouse
+            double left = mousePos.X - (rectando.ActualWidth / 2);
+            double top = mousePos.Y - (rectando.ActualHeight / 2);
+            Canvas.SetLeft(rectando, left);
+            Canvas.SetTop(rectando, top);
+            
+
+        }
+        [DelimitedRecord("|")]
+        public class Orders
+        {
+            public int x;
+            public int y;
+
+
+        }
+
+
     }
 }
